f1c10651b91da25af57093077e883edd04e1f896
Alvaro-Cabrera <alvaro.cabrera@bootcamp-upgrade.com>
2023-07-18 17:04:07 +0200

    pokedex cambios

--- pokedex.js
+++ pokedex.js
@@ -6,12 +6,12 @@ document.addEventListener("DOMContentLoaded", function () {
     async function fetchPokemonData() {
       for (let i = 1; i <= 150; i++) {
         const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`);
-        const data = await response.json();
+        const dato = await response.json();
         const pokemon = {
-          name: data.name,
-          image: data.sprites.front_default,
-          type: data.types.map((type) => type.type.name).join(", "),
-          id: data.id
+          name: dato.name,
+          image: dato.sprites.front_default,
+          type: dato.types.map((type) => type.type.name).join(", "),
+          id: dato.id
         };
         
         displayPokemon(pokemon);
@@ -19,30 +19,31 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   
     function displayPokemon(pokemon) {
-      const card = document.createElement("li");
-      card.classList.add("card");
+      const carta = document.createElement("li");
+      carta.classList.add("card");
+      
   
-      const cardTitle = document.createElement("h2");
-      cardTitle.classList.add("card-title");
-      cardTitle.innerText = pokemon.name;
+      const cardTitulo = document.createElement("h2");
+      cardTitulo.classList.add("card-title");
+      cardTitulo.innerText = pokemon.name;
   
-      const cardSubtitle = document.createElement("h3");
-      cardSubtitle.classList.add("card-subtitle");
-      cardSubtitle.innerText = `#${pokemon.id}`;
+      const cardSubtitulo = document.createElement("h3");
+      cardSubtitulo.classList.add("card-subtitle");
+      cardSubtitulo.innerText = `Nº ${pokemon.id}`;
   
-      const cardImage = document.createElement("img");
-      cardImage.classList.add("card-image");
-      cardImage.src = pokemon.image;
+      const cardImagen = document.createElement("img");
+      cardImagen.classList.add("card-image");
+      cardImagen.src = pokemon.image;
   
-      const cardType = document.createElement("p");
-      cardType.innerText = `Type: ${pokemon.type}`;
+      const cardTipo = document.createElement("p");
+      cardTipo.innerText = `Type: ${pokemon.type}`;
   
-      card.appendChild(cardTitle);
-      card.appendChild(cardSubtitle);
-      card.appendChild(cardImage);
-      card.appendChild(cardType);
+      carta.appendChild(cardTitulo);
+      carta.appendChild(cardSubtitulo);
+      carta.appendChild(cardImagen);
+      carta.appendChild(cardTipo);
   
-      pokedex.appendChild(card);
+      pokedex.appendChild(carta);
     }
   });
   
